d8f91d3ccd5c19b974f14716a232bacd44934394
yk <yosizuka1@gmail.com>
2015-05-31 23:55:10 +0900

    テーブル追加 storeinfo roominfo #4

--- sql/sqlite.sql
+++ sql/sqlite.sql
@@ -56,6 +56,8 @@ VALUES
 ('しろくま','しろくま','2013-03-29'),
 ('nook','nook','2013-03-29');
 
+DROP TABLE general;
+
 CREATE TABLE general (                              -- 一般ユーザー
     id          INTEGER PRIMARY KEY AUTOINCREMENT,  -- 一般ユーザーID
     login       TEXT,                               -- ログインID名
@@ -112,6 +114,8 @@ VALUES
 ('しろくま','しろくま','2013-03-29'),
 ('nook','nook','2013-03-29');
 
+DROP TABLE profile;
+
 CREATE TABLE profile (                                  -- 個人情報
     id              INTEGER PRIMARY KEY AUTOINCREMENT,  -- 個人情報ID
     general_id      INTEGER,                            -- 一般ユーザーID
@@ -134,3 +138,45 @@ VALUES
 ('','1','やまがた山縣','山縣有朋','やまがた ありとも','098-000-0997','foo@gmail.com','1','2015-05-23','2015-05-23'),
 ('','2','まつかた松方','松方正義','まつかた まさよし','098-000-0997','foo@gmail.com','1','2015-05-23','2015-05-23'),
 ('','3','おおくま大隈','大隈重信','おおくま しげのぶ','098-000-0997','foo@gmail.com','1','2015-05-23','2015-05-23');
+
+CREATE TABLE storeinfo (                                    -- 店舗情報
+    id                  INTEGER PRIMARY KEY AUTOINCREMENT,  -- 店舗ID
+    region_id           INTEGER,                            -- 地域区分ID
+    admin_id            INTEGER,                            -- 管理ユーザーID
+    name                TEXT,                               -- 店舗名
+    icon                TEXT,                               -- 店舗アイコン
+    post                TEXT,                               -- 住所郵便
+    state               TEXT,                               -- 住所都道府県
+    cities              TEXT,                               -- 住所市町村
+    addressbelow        TEXT,                               -- 住所以下
+    tel                 TEXT,                               -- 電話番号
+    mail                TEXT,                               -- メールアドレス
+    remarks             TEXT,                               -- 店舗備考欄
+    url                 TEXT,                               -- 店舗リンク先
+    locationinfor       TEXT,                               -- 地図位置情報
+    status              INTEGER,                            -- ステータス
+    create_on           TEXT,                               -- 登録日
+    modify_on           TEXT                                -- 修正日
+);
+
+CREATE TABLE roominfo(                                      -- 部屋情報設定
+    id                   INTEGER PRIMARY KEY AUTOINCREMENT, -- 部屋情報ID
+    storeinfo_id         INTEGER,                           -- 店舗ID
+    name                 TEXT,                              -- 部屋名
+    starttime_on         TEXT,                              -- 開始時刻
+    endingtime_on        TEXT,                              -- 終了時刻
+    rentalunit           INTEGER,                           -- 貸出単位
+    time_change          INTEGER,                           -- 開始時間切り替え
+    pricescomments       TEXT,                              -- 料金コメント
+    privatepermit        INTEGER,                           -- 個人練習許可設定
+    privatepeople        INTEGER,                           -- 個人練習許可人数
+    privateconditions    INTEGER,                           -- 個人練習許可条件
+    bookinglimit         INTEGER,                           -- 予約制限
+    cancellimit          INTEGER,                           -- キャンセル制限
+    remarks              TEXT,                              -- 備考
+    webpublishing        INTEGER,                           -- web公開設定
+    webreserve           INTEGER,                           -- web予約受付設定
+    status               INTEGER,                           -- ステータス
+    create_on            TEXT,                              -- 登録日
+    modify_on            TEXT                               -- 修正日
+);
