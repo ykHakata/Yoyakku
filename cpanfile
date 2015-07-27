795b39f717245c2d24717c28f76246917f576e4c
yk <yosizuka1@gmail.com>
2015-07-27 21:51:43 +0900

    Class::Accessor::Fast 導入 #4

--- cpanfile
+++ cpanfile
@@ -5,3 +5,4 @@ requires 'FormValidator::Lite', '== 0.38';
 requires 'HTML::FillInForm', '== 2.21';
 requires 'Teng', '== 0.28';
 requires 'DBD::SQLite', '1.46';
+requires 'Class::Accessor::Fast', '== 0.34';
--- lib/Yoyakku/Model.pm
+++ lib/Yoyakku/Model.pm
@@ -5,6 +5,12 @@ use utf8;
 use Teng;
 use Teng::Schema::Loader;
 use FormValidator::Lite qw{Email URL DATE TIME};
+use base qw{Class::Accessor::Fast};
+
+__PACKAGE__->mk_accessors(
+    qw{params session method html login_row login_table
+        login_name profile_row storeinfo_row template}
+);
 
 =encoding utf8
 
@@ -21,103 +27,6 @@ This documentation referes to Yoyakku::Model version 0.0.1
     データベース接続関連の API を提供
 
 =cut
-
-sub new {
-    my $class  = shift;
-    my $params = +{};
-    my $self   = bless $params, $class;
-    return $self;
-}
-
-sub params {
-    my $self   = shift;
-    my $params = shift;
-    if ($params) {
-        $self->{params} = $params;
-    }
-    return $self->{params};
-}
-
-sub session {
-    my $self    = shift;
-    my $session = shift;
-    if ($session) {
-        $self->{session} = $session;
-    }
-    return $self->{session};
-}
-
-sub method {
-    my $self   = shift;
-    my $method = shift;
-    if ($method) {
-        $self->{method} = $method;
-    }
-    return $self->{method};
-}
-
-sub html {
-    my $self = shift;
-    my $html = shift;
-    if ($html) {
-        $self->{html} = $html;
-    }
-    return $self->{html};
-}
-
-sub login_row {
-    my $self      = shift;
-    my $login_row = shift;
-    if ($login_row) {
-        $self->{login_row} = $login_row;
-    }
-    return $self->{login_row};
-}
-
-sub login_table {
-    my $self        = shift;
-    my $login_table = shift;
-    if ($login_table) {
-        $self->{login_table} = $login_table;
-    }
-    return $self->{login_table};
-}
-
-sub login_name {
-    my $self       = shift;
-    my $login_name = shift;
-    if ($login_name) {
-        $self->{login_name} = $login_name;
-    }
-    return $self->{login_name};
-}
-
-sub profile_row {
-    my $self        = shift;
-    my $profile_row = shift;
-    if ($profile_row) {
-        $self->{profile_row} = $profile_row;
-    }
-    return $self->{profile_row};
-}
-
-sub storeinfo_row {
-    my $self          = shift;
-    my $storeinfo_row = shift;
-    if ($storeinfo_row) {
-        $self->{storeinfo_row} = $storeinfo_row;
-    }
-    return $self->{storeinfo_row};
-}
-
-sub template {
-    my $self     = shift;
-    my $template = shift;
-    if ($template) {
-        $self->{template} = $template;
-    }
-    return $self->{template};
-}
 
 =head2 get_storeinfo_rows_all
 
@@ -263,6 +172,10 @@ __END__
 =item * L<Teng::Schema::Loader>
 
 =item * L<FormValidator::Lite>
+
+=item * L<base>
+
+=item * L<Class::Accessor::Fast>
 
 =back
 
