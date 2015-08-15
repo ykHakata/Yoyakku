4214c15da683a35390938b2d6136b5a0530dfa9e
yk <yosizuka1@gmail.com>
2015-08-15 18:33:07 +0900

    修正 #20

--- lib/Yoyakku/Model/Entry.pm
+++ lib/Yoyakku/Model/Entry.pm
@@ -13,14 +13,8 @@ use Yoyakku::Util qw{now_datetime get_fill_in_params};
 
 sub get_header_stash_entry {
     my $self = shift;
-
-    my $table      = $self->login_table();
-    my $login_row  = $self->login_row();
-    my $login_name = $self->login_name();
-
     my $switch_header = 2;
-
-    return $self->get_header_stash_params( $switch_header, $login_name );
+    return $self->get_header_stash_params( $switch_header );
 }
 
 =head2 get_ads_navi_rows
