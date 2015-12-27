6992533281ecce40ddb65538f9d2ba5b2006b6c4
yk <yosizuka1@gmail.com>
2015-12-28 00:20:52 +0900

    update機能移植 #22

--- lib/Yoyakku/Controller/Setting/Roominfo.pm
+++ lib/Yoyakku/Controller/Setting/Roominfo.pm
@@ -79,12 +79,18 @@ sub _update {
     my $self  = shift;
     my $model = shift;
 
+    $model->type('update');
+
     my $check_params = $model->get_check_params_list();
 
     for my $check_param ( @{$check_params} ) {
         my $valid_msg = $model->check_validator( 'roominfo', $check_param );
         return $self->stash($valid_msg), $self->_render_fill_in_form($model)
             if $valid_msg;
+    }
+
+    for my $check_param ( @{$check_params} ) {
+        $model->writing_admin_reserv($check_param);
     }
 
     return $self->redirect_to('up_admin_r_d_edit');
@@ -125,203 +131,3 @@ __END__
 L<Guides>
 
 =cut
-
-
-
-
-
-
-#admin_reserv_edit.html.ep
-#予約部屋情報設定コントロール-----------------------------
-any '/admin_reserv_edit' => sub {
-my $self = shift;
-# テンプレートbodyのクラス名を定義
-#my $class = "admin_store_comp";
-my $class = "admin_reserv_edit";
-$self->stash(class => $class);
-
-
-
-
-if (uc $self->req->method eq 'POST') {
-    #submitボタン判定、キャンセルの時は空の値、完了の時バリデート
-
-    #入力検査合格、の時、修正アップロード実行
-    if ( $self->validate($validator,$param_hash) ) {
-        #入力値を全部受け取っておく念のために時刻取得
-        my $today = localtime;
-        # 入力フォームから受ける値変数
-        my @id                = $self->param('id');
-        #my @storeinfo_id      = $self->param('storeinfo_id');
-        my @name              = $self->param('name');
-        my @starttime_on      = $self->param('starttime_on');
-        my @endingtime_on     = $self->param('endingtime_on');
-        my @time_change       = $self->param('time_change');
-        my @rentalunit        = $self->param('rentalunit');
-        my @pricescomments    = $self->param('pricescomments');
-        my @privatepermit     = $self->param('privatepermit');
-        my @privatepeople     = $self->param('privatepeople');
-        my @privateconditions = $self->param('privateconditions');
-        #my @bookinglimit      = $self->param('bookinglimit');
-        #my @cancellimit       = $self->param('cancellimit');
-        #my @remarks           = $self->param('remarks');
-#        my @webpublishing     = $self->param('webpublishing');
-#        my @webreserve        = $self->param('webreserve');
-#        my @status            = $self->param('status');
-#        my @create_on         = $self->param('create_on');
-#        my @modify_on         = $today->datetime(date => '-', T => ' ');
-        # 修正日付の配列をつくる
-        my $modify_on = $today->datetime(date => '-', T => ' ');
-        my @modify_on;
-        for my $i (@id) {
-            push (@modify_on,$modify_on);
-        }
-        #nameに空白が含まれた場合取り除いておく
-        my @s_name;
-        foreach my $name (@name) {
-            if ($name =~ /^\s+$/) {
-                $name =~ s/\s+//;
-                push (@s_name,$name);
-            }
-            elsif ($name =~ /\s+./) {
-                $name =~ s/\s+//;
-                push (@s_name,$name);
-            }
-            elsif ($name =~ /.\s+/) {
-                $name =~ s/\s+//;
-                push (@s_name,$name);
-            }
-            else {
-                push (@s_name,$name);
-            }
-        }
-        my @name = @s_name;
-
-        #sql書き込む前に開始、終了時刻変換
-        my @conver_starttime_on;
-        foreach my $starttime_on (@starttime_on) {
-            if ($starttime_on =~ /^[2][4-9]$/) {
-                $starttime_on -= 24;
-                $starttime_on .= ":00";
-                push (@conver_starttime_on,$starttime_on);
-            } else {
-                $starttime_on .= ":00";
-                push (@conver_starttime_on,$starttime_on);
-            }
-        }
-        my @starttime_on = @conver_starttime_on;
-
-        my @conver_endingtime_on;
-        foreach my $endingtime_on (@endingtime_on) {
-            if ($endingtime_on =~ /^[2][4-9]$|^[3][0]$/) {
-                $endingtime_on -= 24;
-                $endingtime_on .= ":00";
-                push (@conver_endingtime_on,$endingtime_on);
-            } else {
-                $endingtime_on .= ":00";
-                push (@conver_endingtime_on,$endingtime_on);
-            }
-        }
-        my @endingtime_on = @conver_endingtime_on;
-
-        my $name             ;
-        my $starttime_on     ;
-        my $endingtime_on    ;
-        my $time_change      ;
-        my $rentalunit       ;
-        my $pricescomments   ;
-        my $privatepermit    ;
-        my $privatepeople    ;
-        my $privateconditions;
-        #my $bookinglimit     ;
-        #my $cancellimit      ;
-        #my $remarks          ;
-        my $modify_on        ;
-        # $idのあるだけ繰り返しsqlへアップデート
-        foreach my $id (@id) {
-            $name              = shift @name             ;
-            $starttime_on      = shift @starttime_on     ;
-            $endingtime_on     = shift @endingtime_on    ;
-            $time_change       = shift @time_change      ;
-            $rentalunit        = shift @rentalunit       ;
-            $pricescomments    = shift @pricescomments   ;
-            $privatepermit     = shift @privatepermit    ;
-            $privatepeople     = shift @privatepeople    ;
-            $privateconditions = shift @privateconditions;
-            #$bookinglimit      = shift @bookinglimit     ;
-            #$cancellimit       = shift @cancellimit      ;
-            #$remarks           = shift @remarks          ;
-            $modify_on         = shift @modify_on        ;
-            #name(部屋名)が存在するときだけ書き込みするように
-            if ($name) {
-                my $count = $teng->update( #修正データをsqlへ送り込み,status->1(利用開始)
-                    'roominfo' => {
-                        #'storeinfo_id'      => $storeinfo_id,
-                        'name'              => $name,
-                        'starttime_on'      => $starttime_on,
-                        'endingtime_on'     => $endingtime_on,
-                        'time_change'       => $time_change,
-                        'rentalunit'        => $rentalunit,
-                        'pricescomments'    => $pricescomments,
-                        'privatepermit'     => $privatepermit,
-                        'privatepeople'     => $privatepeople,
-                        'privateconditions' => $privateconditions,
-                        #'bookinglimit'      => $bookinglimit,
-                        #'cancellimit'       => $cancellimit,
-                        #'remarks'           => $remarks,
-                        #'webpublishing'     => $webpublishing,
-                        #'webreserve'        => $webreserve,
-                        'status'            => 1,
-                        #'create_on'         => $create_on,
-                        'modify_on'         => $modify_on,
-                    },{
-                        'id' => $id,
-                    }
-                );
-            }
-            #name無いときはstatus->0(利用不可)にしておく
-            else {
-                my $count = $teng->update(
-                    'roominfo' => {
-                        #'storeinfo_id'      => $storeinfo_id,
-                        'name'              => $name,
-                        'starttime_on'      => $starttime_on,
-                        'endingtime_on'     => $endingtime_on,
-                        'time_change'       => $time_change,
-                        'rentalunit'        => $rentalunit,
-                        'pricescomments'    => $pricescomments,
-                        'privatepermit'     => $privatepermit,
-                        'privatepeople'     => $privatepeople,
-                        'privateconditions' => $privateconditions,
-                        #'bookinglimit'      => $bookinglimit,
-                        #'cancellimit'       => $cancellimit,
-                        #'remarks'           => $remarks,
-                        #'webpublishing'     => $webpublishing,
-                        #'webreserve'        => $webreserve,
-                        'status'            => 0,
-                        #'create_on'         => $create_on,
-                        'modify_on'         => $modify_on,
-                    },{
-                        'id' => $id,
-                    }
-                );
-            }
-        }
-        #sqlにデータ入力したのでリダイレクト
-        return $self->redirect_to('up_admin_r_d_edit');
-        #リターンなのでここでおしまい。
-    }
-    #入力検査合格しなかった場合、もう一度入力フォーム表示Fillinにて
-    my $html = $self->render_partial()->to_string;
-    $html = HTML::FillInForm->fill(\$html, $self->req->params,);
-    return $self->render_text($html, format => 'html');
-    #リターンなのでここでおしまい。
-    }
-}
-#get入力、そのままfillinでrender
-else {
-    #Fillin画面表示実行returnなのでここでおしまい。
-    return $self->render_text($html, format => 'html');
-}
-
-};
--- lib/Yoyakku/Model/Setting/Roominfo.pm
+++ lib/Yoyakku/Model/Setting/Roominfo.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use utf8;
 use parent 'Yoyakku::Model::Setting';
-use Yoyakku::Util qw{get_fill_in_params chenge_time_over};
+use Yoyakku::Util qw{get_fill_in_params chenge_time_over join_time};
 
 =encoding utf8
 
@@ -59,15 +59,19 @@ sub set_roominfo_params {
 
     my $roominfo_ref = +{};
     for my $row ( @{$rows} ) {
-        my $change_time = chenge_time_over(
+        my $params  = +{};
+        my $split_t = chenge_time_over(
             +{  start_time => $row->starttime_on,
                 end_time   => $row->endingtime_on,
             },
         );
+        ( $params->{starttime_on}, $params->{endingtime_on}, )
+            = join_time( $split_t, 'none' );
+
         push @{ $roominfo_ref->{id} },             $row->id;
         push @{ $roominfo_ref->{name} },           $row->name;
-        push @{ $roominfo_ref->{starttime_on} },   $change_time->{start_hour};
-        push @{ $roominfo_ref->{endingtime_on} },  $change_time->{end_hour};
+        push @{ $roominfo_ref->{starttime_on} },   $params->{starttime_on};
+        push @{ $roominfo_ref->{endingtime_on} },  $params->{endingtime_on};
         push @{ $roominfo_ref->{time_change} },    $row->time_change;
         push @{ $roominfo_ref->{rentalunit} },     $row->rentalunit;
         push @{ $roominfo_ref->{pricescomments} }, $row->pricescomments;
@@ -115,6 +119,70 @@ sub get_check_params_list {
     }
 
     return $check_params;
+}
+
+=head2 writing_admin_reserv
+
+    roominfo テーブル書込み、修正に対応
+
+=cut
+
+sub writing_admin_reserv {
+    my $self   = shift;
+    my $params = shift;
+
+    # 書き込む前に開始、終了時刻変換
+    my $FIELD_SEPARATOR_TIME = q{:};
+    my $FIELD_COUNT_TIME     = 3;
+
+    my ( $start_hour, $start_minute, $start_second, )
+        = split $FIELD_SEPARATOR_TIME, $params->{starttime_on},
+        $FIELD_COUNT_TIME + 1;
+
+    my ( $end_hour, $end_minute, $end_second, ) = split $FIELD_SEPARATOR_TIME,
+        $params->{endingtime_on}, $FIELD_COUNT_TIME + 1;
+
+    # 数字にもどす
+    $start_hour += 0;
+    $end_hour   += 0;
+
+    # 時間の表示を変換
+    if ( $start_hour >= 24 && $start_hour <= 30 ) {
+        $start_hour -= 24;
+    }
+
+    if ( $end_hour >= 24 && $end_hour <= 30 ) {
+        $end_hour -= 24;
+    }
+
+    $params->{starttime_on} = join ':', $start_hour, $start_minute,
+        $start_second;
+    $params->{endingtime_on} = join ':', $end_hour, $end_minute, $end_second;
+
+    $params->{starttime_on}  = sprintf '%08s', $params->{starttime_on};
+    $params->{endingtime_on} = sprintf '%08s', $params->{endingtime_on};
+
+    my $create_data = $self->get_create_data( 'roominfo', $params );
+
+    # 不要なカラムを削除
+    delete $create_data->{storeinfo_id};
+    delete $create_data->{bookinglimit};
+    delete $create_data->{cancellimit};
+    delete $create_data->{remarks};
+    delete $create_data->{webpublishing};
+    delete $create_data->{webreserve};
+
+    # name (部屋名) が存在するときだけ status 1 (利用可能)
+    $create_data->{status} = 0;
+    if ( $create_data->{name} ) {
+        $create_data->{status} = 1;
+    }
+
+    # update 以外は禁止
+    die 'update only'
+        if !$self->type() || ( $self->type() && $self->type() ne 'update' );
+
+    return $self->writing_db( 'roominfo', $create_data, $params->{id} );
 }
 
 1;
