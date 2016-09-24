622f083bd277ad67bf682defb210ed6562013b6e
yk <yosizuka1@gmail.com>
2016-09-25 01:05:40 +0900

    route 記述変更 #40

--- lib/Yoyakku.pm
+++ lib/Yoyakku.pm
@@ -32,109 +32,104 @@ sub startup {
     my $r = $self->routes;
 
     # トップページ遷移(/)
-    $r->route('/')->to( controller => 'Calendar', action => 'index' );
+    $r->route('/')->to('Calendar#index');
 
-    # ログインフォーム 入り口, 一般, 店舗管理者, スーパーユーザー, ログアウト
-    my $auth
-        = qr{up_login\z|up_login_general|up_login_admin|root_login|up_logout};
-    $r->route( '/:auth', auth => $auth )
-        ->to( controller => 'Auth', action => 'index' );
+    # 一般, 店舗管理者, ログインフォーム 入り口, スーパーユーザー, ログアウト
+    $r->route('up_login_general')->to('Auth#index');
+    $r->route('up_login_admin')->to('Auth#index');
+    $r->route('up_login')->to('Auth#index');
+    $r->route('root_login')->to('Auth#index');
+    $r->route('up_logout')->to('Auth#index');
 
-    # 個人情報 入力画面, 確認画面
-    my $profile = qr{profile\z|profile_comp};
-    $r->route( '/:profile', profile => $profile )
-        ->to( controller => 'profile', action => 'index' );
+    # 個人情報 確認画面, 入力画面
+    $r->route('/profile_comp')->to('Profile#index');
+    $r->route('/profile')->to('Profile#index');
 
     # システム管理者のオープニング画面
     my $mainte = qr{mainte_list};
-    $r->route( '/:mainte', mainte => $mainte )
-        ->to( controller => 'mainte', action => 'index' );
+    $r->route( '/:mainte', mainte => $mainte )->to('Mainte#index');
 
     # システム管理者(admin) 新規 編集
     my $mainte_admin = qr{mainte_registrant_serch|mainte_registrant_new};
     $r->route( '/:mainte_admin', mainte_admin => $mainte_admin )
-        ->to( controller => 'Mainte::Admin', action => 'index' );
+        ->to('Mainte::Admin#index');
 
     # システム管理者(general) 新規 編集
     my $mainte_general = qr{mainte_general_serch\z|mainte_general_new\z};
     $r->route( '/:mainte_general', mainte_general => $mainte_general )
-        ->to( controller => 'Mainte::General', action => 'index' );
+        ->to('Mainte::General#index');
 
     # システム管理者(profile) 新規 編集
     my $mainte_profile = qr{mainte_profile_serch\z|mainte_profile_new\z};
     $r->route( '/:mainte_profile', mainte_profile => $mainte_profile )
-        ->to( controller => 'Mainte::Profile', action => 'index' );
+        ->to('Mainte::Profile#index');
 
     # システム管理者(storeinfo) 新規 編集
     my $mainte_storeinfo
         = qr{mainte_storeinfo_serch\z|mainte_storeinfo_new\z};
     $r->route( '/:mainte_storeinfo', mainte_storeinfo => $mainte_storeinfo )
-        ->to( controller => 'Mainte::Storeinfo', action => 'index' );
+        ->to('Mainte::Storeinfo#index');
 
     # システム管理者(roominfo) 新規 編集
     my $mainte_roominfo = qr{mainte_roominfo_serch\z|mainte_roominfo_new\z};
     $r->route( '/:mainte_roominfo', mainte_roominfo => $mainte_roominfo )
-        ->to( controller => 'Mainte::Roominfo', action => 'index' );
+        ->to('Mainte::Roominfo#index');
 
     # システム管理者(reserve) 新規 編集
     my $mainte_reserve = qr{mainte_reserve_serch\z|mainte_reserve_new\z};
     $r->route( '/:mainte_reserve', mainte_reserve => $mainte_reserve )
-        ->to( controller => 'Mainte::Reserve', action => 'index' );
+        ->to('Mainte::Reserve#index');
 
     # システム管理者(acting) 新規 編集
     my $mainte_acting = qr{mainte_acting_serch\z|mainte_acting_new\z};
     $r->route( '/:mainte_acting', mainte_acting => $mainte_acting )
-        ->to( controller => 'Mainte::Acting', action => 'index' );
+        ->to('Mainte::Acting#index');
 
     # システム管理者(ads) 新規 編集
     my $mainte_ads = qr{mainte_ads_serch\z|mainte_ads_new\z};
     $r->route( '/:mainte_ads', mainte_ads => $mainte_ads )
-        ->to( controller => 'Mainte::Ads', action => 'index' );
+        ->to('Mainte::Ads#index');
 
     # システム管理者(region) 新規 編集
     my $mainte_region = qr{mainte_region_serch\z|mainte_region_new\z};
     $r->route( '/:mainte_region', mainte_region => $mainte_region )
-        ->to( controller => 'Mainte::Region', action => 'index' );
+        ->to('Mainte::Region#index');
 
     # システム管理者(post) 新規 編集
     my $mainte_post = qr{mainte_post_serch\z|mainte_post_new\z};
     $r->route( '/:mainte_post', mainte_post => $mainte_post )
-        ->to( controller => 'Mainte::Post', action => 'index' );
+        ->to('Mainte::Post#index');
 
     # オープニングカレンダー, 今月, 1ヶ月後, 2ヶ月後, 3ヶ月後
     my $cal
         = qr{index\z|index_next_m\z|index_next_two_m\z|index_next_three_m\z};
-    $r->route( '/:cal', cal => $cal )
-        ->to( controller => 'Calendar', action => 'index' );
+    $r->route( '/:cal', cal => $cal )->to('Calendar#index');
 
     # 登録(entry)
-    $r->route( '/:entry', entry => qr{entry} )
-        ->to( controller => 'Entry', action => 'index' );
+    $r->route('/entry')->to('Entry#index');
 
     # 予約(region)
-    $r->route( '/:region', region => qr{region_state} )
-        ->to( controller => 'Region', action => 'index' );
+    $r->route('/region_state')->to('Region#index');
 
     # 店舗管理(Management) 選択店舗情報確認
-    my $management_storeinfo = qr{admin_store_edit|admin_store_comp};
-    $r->route( '/:store', store => $management_storeinfo )
-        ->to( controller => 'Management::Storeinfo', action => 'index' );
+    $r->route('/admin_store_edit')->to('Management::Storeinfo#index');
+    $r->route('/admin_store_comp')->to('Management::Storeinfo#index');
 
     # 店舗管理(Management) 予約部屋情報設定
-    my $management_roominfo = qr{admin_reserv_edit|up_admin_r_d_edit|admin_reserv_comp|admin_pub_edit|admin_pub_comp};
-    $r->route( '/:room', room => $management_roominfo )
-        ->to( controller => 'Management::Roominfo', action => 'index' );
+    $r->route('/admin_reserv_edit')->to('Management::Roominfo#index');
+    $r->route('/up_admin_r_d_edit')->to('Management::Roominfo#index');
+    $r->route('/admin_reserv_comp')->to('Management::Roominfo#index');
+    $r->route('/admin_pub_edit')->to('Management::Roominfo#index');
+    $r->route('/admin_pub_comp')->to('Management::Roominfo#index');
 
     # 店舗管理(Management) 管理者予約
-    my $management_reserve = qr{admin_reserv_list};
-    $r->route( '/:reserve', reserve => $management_reserve )
-        ->to( controller => 'Management::Reserve', action => 'index' );
+    $r->route('/admin_reserv_list')->to('Management::Reserve#index');
 
     # 紹介ページ
-    $r->route('/tutorial')->to( controller => 'Tutorial', action => 'index' );
+    $r->route('/tutorial')->to('Tutorial#index');
 
     # スマホに特化した予約確認画面
-    $r->route('/simple_res')->to( controller => 'Simple', action => 'index' );
+    $r->route('/simple_res')->to('Simple#index');
 
     # セッション情報設定
     $self->sessions->cookie_name('yoyakku');
